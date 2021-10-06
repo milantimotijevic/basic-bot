b0f532bcaa5b76460fbac634d558d38273720876
mtimo101289@gmail.com <mtimo101289@gmail.com>
2021-10-06 23:49:41 +0200

    implement breakout

--- service/action.js
+++ service/action.js
@@ -10,17 +10,36 @@ const callToArms = (guildId) => {
         return;
     }
 
-    guild.members.cache.forEach(async (member) => {
+    guild.members.cache.forEach((member) => {
        
         if (!member.user.bot) {
-           await member.voice.setChannel(soldierChannel.id)
+            member.voice.setChannel(soldierChannel.id)
         }
         
     })
 };
 
-const breakOut = (guildName) => {
-    console.log('Issuing BreakOut command...');
+const breakOut = (guildId) => {
+    const guild = state.client.guilds.cache.get(guildId);
+    guild.members.cache.forEach((member) => {
+
+        let role;
+        if (member && !member.user.bot && member.roles && member.roles.cache) {
+            member.roles.cache.forEach((r) => {
+                if (r && r.name && r.name !== '@everyone') {
+                    role = r;
+                    return;
+                }
+            });
+
+            const targetChannel = getChannelByName(guild, role.name);
+            if (!targetChannel || !targetChannel.id) {
+                console.log(`Failed Break Out for member "${member.user.id}", channel "${role.name}" not found`);
+            }
+
+            member.voice.setChannel(targetChannel.id)
+        }
+    });
 };
 
 module.exports = {
